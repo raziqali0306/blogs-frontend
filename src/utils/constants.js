7700583abac863d4ee02673d11c1d5637e34b3c1
raziqali0306 <raziqali0306@gmail.com>
2022-12-09 13:27:27 +0530

    fix: changes base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 // Uncomment to use production server.
 // export const BASE_URL = "https://blogs-backend.up.railway.app" // production@railway.app 
-export const BASE_URL = "http://172.105.40.129" // production@LinodeVM
+export const BASE_URL = "http://172.105.40.129/api" // production@LinodeVM
 
 // Uncomment to use local server.
-// export const BASE_URL = "http://localhost:3000"
+// export const BASE_URL = "http://localhost:3000/api"
