60182332dc64187e7019fba8021161b878a78023
raziqali0306 <raziqali0306@gmail.com>
2023-02-10 13:32:24 +0530

    changes backend api to railway app

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 // Uncomment to use production server.
-export const BASE_URL = "https://blogs-backend.up.railway.app"; // production@railway.app
+export const BASE_URL = "https://blogs-backend.up.railway.app/api"; // production@railway.app
 // export const BASE_URL = "http://172.105.40.129/api" // production@LinodeVM
 
 // Uncomment to use local server.
